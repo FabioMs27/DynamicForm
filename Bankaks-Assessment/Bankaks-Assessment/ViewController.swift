eea217fd37d168de1d802dae1df99eb143961d1b
Fábio Maciel de Sousa <fabio.ms27@hotmail.com>
2020-11-29 16:39:55 -0300

    Implement api request

--- Bankaks-Assessment/Bankaks-Assessment/ViewController.swift
+++ Bankaks-Assessment/Bankaks-Assessment/ViewController.swift
@@ -11,9 +11,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchFormType(option: 3)
     }
-
-
+    
+    func fetchFormType(option: Int){
+        DispatchQueue.global(qos: .background).async{
+            guard let url = URL(string: "https://api-staging.bankaks.com/task/\(option)") else { return }
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            URLSession.shared.dataTask(with: request){ (data,response,err) in
+                if let data = data{
+                    let result = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
+                    print(result as Any)
+                }
+            }.resume()
+        }
+    }
 }
-
