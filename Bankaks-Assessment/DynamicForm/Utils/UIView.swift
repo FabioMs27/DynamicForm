0baff7ce59af1d8189abe0a4aef679799cb28687
Fábio Maciel de Sousa <fabio.ms27@hotmail.com>
2021-03-11 12:54:21 +0100

    Add LayoutAnchor

--- Bankaks-Assessment/DynamicForm/Utils/UIView.swift
+++ Bankaks-Assessment/DynamicForm/Utils/UIView.swift
@@ -18,4 +18,46 @@ extension UIView{
         animation.toValue = NSValue(cgPoint: CGPoint(x: center.x + 10, y: center.y))
         layer.add(animation, forKey: "position")
     }
+    
+    func addSubview(_ subview: UIView, anchors: [LayoutAnchor]) {
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(subview)
+        subview.activate(anchors: anchors, relativeTo: self)
+    }
+
+    func activate(anchors: [LayoutAnchor], relativeTo item: UIView? = nil) {
+        let constraints = anchors.map { NSLayoutConstraint(from: self, to: item, anchor: $0) }
+        NSLayoutConstraint.activate(constraints)
+    }
+}
+
+extension NSLayoutConstraint {
+    convenience init(from: UIView, to item: UIView?, anchor: LayoutAnchor) {
+        switch anchor {
+        case let .constant(attribute: attr, relation: relation, constant: constant):
+            self.init(
+                item: from,
+                attribute: attr,
+                relatedBy: relation,
+                toItem: nil,
+                attribute: .notAnAttribute,
+                multiplier: 1,
+                constant: constant
+            )
+        case let .relative(attribute: attr,
+                           relation: relation,
+                           relatedTo: relatedTo,
+                           multiplier: multiplier,
+                           constant: constant):
+            self.init(
+                item: from,
+                attribute: attr,
+                relatedBy: relation,
+                toItem: item,
+                attribute: relatedTo,
+                multiplier: multiplier,
+                constant: constant
+            )
+        }
+    }
 }
